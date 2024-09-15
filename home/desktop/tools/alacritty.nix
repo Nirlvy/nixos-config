9142c829f7d339c939329a7abf5aa594c6fd614d
Nirlvy <nirlvy@qq.com>
2024-09-15 23:28:59 +0800

    feat: alacritty use catppuccin-frappe theme

--- home/desktop/tools/alacritty.nix
+++ home/desktop/tools/alacritty.nix
@@ -9,28 +9,76 @@
       };
       colors = {
         primary = {
-          background = "#1c1e26";
-          foreground = "#e0e0e0";
+          background = "#303446";
+          foreground = "#c6d0f5";
+          dim_foreground = "#838ba7";
+          bright_foreground = "#c6d0f5";
+        };
+        cursor = {
+          text = "#303446";
+          cursor = "#f2d5cf";
+        };
+        vi_mode_cursor = {
+          text = "#303446";
+          cursor = "#babbf1";
+        };
+        search = {
+          matches = {
+            foreground = "#303446";
+            background = "#a5adce";
+          };
+          focused_match = {
+            foreground = "#303446";
+            background = "#a6d189";
+          };
+        };
+        footer_bar = {
+          foreground = "#303446";
+          background = "#a5adce";
+        };
+        hints = {
+          start = {
+            foreground = "#303446";
+            background = "#e5c890";
+          };
+          end = {
+            foreground = "#303446";
+            background = "#a5adce";
+          };
+        };
+        selection = {
+          text = "#303446";
+          background = "#f2d5cf";
         };
         normal = {
-          black = "#16161c";
-          blue = "#26bbd9";
-          cyan = "#59e1e3";
-          green = "#29d398";
-          magenta = "#ee64ac";
-          red = "#395678";
-          white = "#d5d8da";
-          yellow = "#fab795";
+          black = "#51576d";
+          red = "#e78284";
+          green = "#a6d189";
+          yellow = "#e5c890";
+          blue = "#8caaee";
+          magenta = "#f4b8e4";
+          cyan = "#81c8be";
+          white = "#b5bfe2";
         };
         bright = {
-          black = "#5b5858";
-          blue = "#3fc4de";
-          cyan = "#6be4e6";
-          green = "#3fdaa4";
-          magenta = "#f075b5";
-          red = "#ec6a88";
-          white = "#d5d8da";
-          yellow = "#fbc3a7";
+          black = "#626880";
+          red = "#e78284";
+          green = "#a6d189";
+          yellow = "#e5c890";
+          blue = "#8caaee";
+          magenta = "#f4b8e4";
+          cyan = "#81c8be";
+          white = "#a5adce";
+        };
+        dim = {
+          black = "#51576d";
+          red = "#e78284";
+          green = "#a6d189";
+          yellow = "#e5c890";
+          blue = "#8caaee";
+          magenta = "#f4b8e4";
+          cyan = "#81c8be";
+          white = "#b5bfe2";
         };
       };
       cursor = {
@@ -43,23 +91,17 @@
         size = 10;
         normal = {
           family = "JetBrainsMono Nerd Font";
-          style = "Regular";
-        };
-        bold = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Bold";
-        };
-        bold_italic = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Bold Italic";
-        };
-        italic = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Italic";
         };
       };
       mouse = {
         hide_when_typing = true;
+        bindings = [
+          {
+            mouse = "Right";
+            mods = "Control";
+            action = "Paste";
+          }
+        ];
       };
       selection = {
         save_to_clipboard = true;
@@ -68,7 +110,7 @@
         program = "${pkgs.zsh}/bin/zsh";
       };
       window = {
-        decorations = "full";
+        decorations = "None";
         dynamic_padding = true;
         opacity = 0.8;
         padding = {
