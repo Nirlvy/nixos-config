7e2f1c5e4a7c9ebcd66133ab0b9628134a906b08
Nirlvy <nirlvy@qq.com>
2024-11-03 18:31:57 +0800

    chore: update flake.lock

--- overlays/override.nix
+++ overlays/override.nix
@@ -1,1 +1,12 @@
-_: _: prev: { catppuccin-gtk = prev.catppuccin-gtk.override { accents = [ "sky" ]; }; }
+_: _: prev: {
+  catppuccin-gtk = prev.catppuccin-gtk.override { accents = [ "sky" ]; };
+  fcitx5-configtool = prev.fcitx5-configtool.override { kcmSupport = false; };
+  # Fix yazi build
+  _7zz = prev._7zz.overrideAttrs (old: {
+    makeFlags = (old.makeFlags or [ ]) ++ [ "USE_ASM=" ];
+  });
+  # Fix sddm->weston->neatvnc build
+  weston = prev.weston.override {
+    vncSupport = false;
+  };
+}
