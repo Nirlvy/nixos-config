e70ef7dfc5c83a51b5f5ab6474161aab1332c851
Nirlvy <nirlvy@qq.com>
2024-11-10 12:27:29 +0800

    fix: 7zz and neatvnc has been fixed

--- overlays/override.nix
+++ overlays/override.nix
@@ -1,12 +1,8 @@
 _: _: prev: {
-  catppuccin-gtk = prev.catppuccin-gtk.override { accents = [ "sky" ]; };
-  fcitx5-configtool = prev.fcitx5-configtool.override { kcmSupport = false; };
-  # Fix yazi build
-  _7zz = prev._7zz.overrideAttrs (old: {
-    makeFlags = (old.makeFlags or [ ]) ++ [ "USE_ASM=" ];
-  });
-  # Fix sddm->weston->neatvnc build
-  weston = prev.weston.override {
-    vncSupport = false;
+  catppuccin-gtk = prev.catppuccin-gtk.override {
+    accents = [ "sky" ];
+    size = "compact";
+    tweaks = [ "rimless" ];
   };
+  fcitx5-configtool = prev.fcitx5-configtool.override { kcmSupport = false; };
 }
