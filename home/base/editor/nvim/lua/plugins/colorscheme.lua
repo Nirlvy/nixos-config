fa465721a5b5d2f2f5022c6d44140ba5fd59e67e
Nirlvy <nirlvy@qq.com>
2024-12-01 14:59:50 +0800

    nvim fix colorscheme

---
+++ home/base/editor/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavour = "macchiato",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
--- home/base/editor/nvim/lua/plugins/example.lua
+++ home/base/editor/nvim/lua/plugins/example.lua
@@ -2,38 +2,13 @@
 -- stylua: ignore
 if true then return {} end
 
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = "1000",
-    opts = {
-      flavour = "frappe",
-    },
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-
   -- change trouble config
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
   },
-
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
   {
