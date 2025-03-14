97d4da5cd50883177669d1179c28a590744169eb
Nirlvy <nirlvy@outlook.com>
2025-03-14 10:33:19 +0800

    fix ghostty notice spam, bump inputs

--- home/desktop/programs/terminal/ghostty.nix
+++ home/desktop/programs/terminal/ghostty.nix
@@ -5,6 +5,7 @@
       font-size = 11;
       theme = "catppuccin-frappe";
       background-opacity = 0.8;
+      background-blur = true;
       window-padding-x = 5;
 
       window-decoration = false;
@@ -15,6 +16,8 @@
       clipboard-read = "allow";
       clipboard-write = "allow";
       copy-on-select = "clipboard";
+      # wait #5995 merge
+      app-notifications = "no-clipboard-copy";
       auto-update = "off";
     };
   };
